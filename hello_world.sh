a2d0dda9d48e1e83992ee6319ae1094b9bcd614d
bashir <bashiruzamas@cloudethix.com>
2022-11-05 00:14:20 +0530

    first shell script Hello cloudethix added

---
+++ hello_world.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CMD="4"
+LL="Hello"
+Date=$(date)
+DATE01=$(date)
+PATH=$(pwd)
+echo
+echo "value of CMD is $CMD"
+echo
+echo "value of LL is $LL"
+echo
+echo "Date is $Date"
+echo
+echo "date is $DATE01"
+echo
+echo "I am curruntly present in $PATH location"
+echo
+UU="Hello cloudethix"
+TT="Hello Moto"
+
+echo -e "\nCall UU variable $UU\n"
+echo -e "call TT variable $TT"
+
+#SYSTEM COMMAND PRINTING IN SINGLE LINE
+
+#DATE02=$(date)
+#HOST_NAME=`hostname`
+#WHOAMI=`whoami`
+#UPTIME=`uptime`
+
+#echo -e "Currunt date : $DATE02\nMy\nMy machine hostname is : $HOST_NAMECurruntly i am login as : $WHOAMISystem is up since : $UPTIME"
+
+#echo "Hello, World"
+#echo "Welcome to cloudethix."
+#echo "print todays currunt date `date` and hostname is $(hostname)"
+#echo "show currunt date : $(date) and hostname `hostname`"
+#echo "show system details `uname`"
